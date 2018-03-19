8d8c08a89b644105eeba44889d812973982c02eb
TREVOR-PC\Trevor <thesilverdrake@gmail.com>
2018-03-18 21:17:42 -0600

    Lecture 156: ApplyForceAtLocation() as well as addition of tank track C++ (which was missed by Source tree

---
+++ Battle Tank/BattleTank/Source/BattleTank/Public/TankTracks.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTracks.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTracks : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	//Sets a throttle between -1 and +1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; //Assume 40 tonne (40000 kg), and 10 m/s2 (1g) of acceleration
+};
